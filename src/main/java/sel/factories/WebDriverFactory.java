653b26ac60742e8cde0487e38ca6e92ba746c9a4
landerx <plandzwojczak94@gmail.com>
2018-08-05 14:15:23 +0200

    moved chrome web driver to resources

--- src/main/java/sel/factories/WebDriverFactory.java
+++ src/main/java/sel/factories/WebDriverFactory.java
@@ -3,23 +3,36 @@ package sel.factories;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import properties.AppProperties;
+
+import java.net.URL;
+import java.util.Objects;
 
 @Slf4j
 public class WebDriverFactory {
 
-    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    private static final String WEB_DRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    private static final String WEB_DRIVER_RESOURCE_PATH ="drivers/chromedriver.exe";
 
-    private static void setChromDriverProperty() {
-        System.setProperty(WEBDRIVER_CHROME_DRIVER, AppProperties.WEB_DRIVER_LOCATION.getValue());
+    private static void setChromeDriverProperty(String webDriverPath) {
+        log.info("Web driver is stored in: " + webDriverPath);
+        System.setProperty(WEB_DRIVER_CHROME_DRIVER,webDriverPath);
     }
 
     public WebDriver create() {
         log.info("Creating web driver");
-        setChromDriverProperty();
+        setChromeDriverProperty(getWebDriverPath());
         WebDriver webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         return webDriver;
+    }
+
+    private String getWebDriverPath() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(WEB_DRIVER_RESOURCE_PATH);
+        if(Objects.isNull(resource)){
+            throw new RuntimeException("Web driver isn't founded in " + WEB_DRIVER_RESOURCE_PATH);
+        }
+        return resource.getPath();
     }
 
 }
